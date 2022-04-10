bc2ef8abe33215c5a25e51410ac0aafc889ee85b
Adrián Martínez <adrimrtnz@gmail.com>
2022-04-10 16:35:23 +0200

    Retoques pantalla de inicio

--- POI_UPV/src/navapp/NavApp.java
+++ POI_UPV/src/navapp/NavApp.java
@@ -9,7 +9,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 
 /**
  *
@@ -21,8 +24,13 @@ public class NavApp extends Application{
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("views/LoginScreenView.fxml"));
         
-        Scene scene = new Scene(root);
+        
         stage.setTitle("NavApp - IPCLab 2022");
+        stage.initStyle(StageStyle.UNDECORATED);
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        
         stage.setScene(scene);
         stage.show();
     }
--- POI_UPV/src/navapp/controllers/LoginScreenController.java
+++ POI_UPV/src/navapp/controllers/LoginScreenController.java
@@ -7,7 +7,16 @@ package navapp.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToolBar;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,12 +25,50 @@ import javafx.fxml.Initializable;
  */
 public class LoginScreenController implements Initializable {
 
+    @FXML private Button closeBtn;
+    @FXML private Button minimizeBtn;
+    @FXML private ToolBar toolBar;
+    
+    private double xOffset;
+    private double yOffset;
+    
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        
+        toolBar.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Stage stage = (Stage) toolBar.getScene().getWindow();
+                xOffset = stage.getX() - event.getScreenX();
+                yOffset = stage.getY() - event.getScreenY();
+            }
+        
+        });
+        
+        
+        toolBar.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Stage stage = (Stage) toolBar.getScene().getWindow();
+                stage.setX(event.getScreenX() + xOffset);
+                stage.setY(event.getScreenY() + yOffset);
+            }
+        
+        });
+        
+        closeBtn.setOnAction((ActionEvent event) -> {
+            Platform.exit();
+        });
+        
+        minimizeBtn.setOnAction((ActionEvent event) -> {
+            ((Stage) minimizeBtn.getScene().getWindow()).setIconified(true);
+        });
     }    
+
     
 }
