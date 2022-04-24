aef4916a09e4deaa3fb8f16a81b7f5ed5ccb7d7e
Adrián Martínez <adrimrtnz@gmail.com>
2022-04-24 09:32:54 +0200

    Implementada herramienta ARRASTRAR en Vista del MAPA

--- POI_UPV/src/navapp/controllers/ExercisesScreenController.java
+++ POI_UPV/src/navapp/controllers/ExercisesScreenController.java
@@ -7,7 +7,12 @@ package navapp.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import model.User;
 
 /**
@@ -16,17 +21,51 @@ import model.User;
  * @author Adrián Martínez
  */
 public class ExercisesScreenController implements Initializable {
-
+    
+    @FXML private ToggleGroup tools;
+    @FXML private ToggleGroup opciones;
+    @FXML private ImageView mapImg;
+    
     private User loggedUser;
+    
+    private double initialXTrans;
+    private double initialYTrans;
+    private double baseX;
+    private double baseY;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        setDragTool();
     }
 
 
     public void initializeUser(User user) {
         loggedUser = user;
         System.out.println("Usuario: " + loggedUser.getNickName());
-    }       
+    }
+    
+    public void setDragTool() {
+        
+        mapImg.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                initialXTrans = event.getSceneX();
+                initialYTrans = event.getSceneY();
+                baseX = mapImg.getTranslateX();
+                baseY = mapImg.getTranslateY();
+            }
+        });
+        
+        
+        mapImg.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                double despX = event.getSceneX() - initialXTrans;
+                double despY = event.getSceneY() - initialYTrans;
+                mapImg.setTranslateX(baseX + despX);
+                mapImg.setTranslateY(baseY + despY);
+                event.consume();
+            }
+        });
+    }
 }
