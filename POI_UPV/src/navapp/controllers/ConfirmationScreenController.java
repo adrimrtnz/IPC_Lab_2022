d593066ba084835e73e8d0afd6b23c75538c4c58
Adrián Martínez <adrimrtnz@gmail.com>
2022-04-27 14:57:58 +0200

    Cambios visuales ventana confirmación.

--- POI_UPV/src/navapp/controllers/ConfirmationScreenController.java
+++ POI_UPV/src/navapp/controllers/ConfirmationScreenController.java
@@ -9,6 +9,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -17,6 +18,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToolBar;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -29,21 +31,18 @@ import model.User;
  */
 public class ConfirmationScreenController implements Initializable {
 
-    @FXML
-    private ToolBar toolBar;
-    @FXML
-    private Button closeBtn;
-    @FXML
-    private Button maximizeButton;
-    @FXML
-    private Button minimizeBtn;
-    @FXML
-    private Button acceptBtn;
-    @FXML
-    private Button cancelBtn;
+    @FXML private ToolBar toolBar;
+    @FXML private Button closeBtn;
+    @FXML private Button maximizeButton;
+    @FXML private Button minimizeBtn;
+    @FXML private Button acceptBtn;
+    @FXML private Button cancelBtn;
     
     private User loggedUser;
     private Boolean closeSession;
+    
+    private double xOffset;
+    private double yOffset;
 
     /**
      * Initializes the controller class.
@@ -73,6 +72,26 @@ public class ConfirmationScreenController implements Initializable {
         
         minimizeBtn.setOnAction((ActionEvent event) -> {
             ((Stage) minimizeBtn.getScene().getWindow()).setIconified(true);
+        });
+        
+        toolBar.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Stage stage = (Stage) toolBar.getScene().getWindow();
+                xOffset = stage.getX() - event.getScreenX();
+                yOffset = stage.getY() - event.getScreenY();
+            }
+        
+        });
+        
+        toolBar.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Stage stage = (Stage) toolBar.getScene().getWindow();
+                stage.setX(event.getScreenX() + xOffset);
+                stage.setY(event.getScreenY() + yOffset);
+            }
+        
         });
         
         
